1294af6271edba510dabc016055954e3d9bc3332
Revanescence <jdangagne@gmail.com>
2022-04-18 19:32:52 -0400

    test

--- P_One_API/P_One_API/Program.cs
+++ P_One_API/P_One_API/Program.cs
@@ -3,7 +3,7 @@ using P_One.Logic;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//testing stuff
+
 //more tests
 //Connection for localhost testing
 //string connectionString = builder.Configuration["connectionString"];
